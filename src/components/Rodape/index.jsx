963d014386b23986d1ecb84afcf0daf2326af6bc
Rôana Góes <roana.goes@gmail.com>
2024-02-16 09:52:39 -0300

    oitava vez

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,3 +1,5 @@
+import { faEnvelopeSquare, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./styles.css";
 
 const Rodape = ({ mensagem }) => {
@@ -9,16 +11,20 @@ const Rodape = ({ mensagem }) => {
             
             <ol id="contato">
                 
-                    
+                <li>
+                    <FontAwesomeIcon icon={faEnvelopeSquare} />
                     <a href="malito:roana.goes@gmail.com" target="_blank">roana.goes@gmail.com </a> <br/><br/>
+                </li>
                 
+                <li>
+                    <FontAwesomeIcon icon={faLinkedin} />
+                    <a href="https://www.linkedin.com/in/r%C3%B4ana-g%C3%B3es-679901206/" target="_blank">Rôana Góes</a> <br/><br/>
+                </li>
                 
-                    
-                     <a href="https://www.linkedin.com/in/r%C3%B4ana-g%C3%B3es-679901206/" target="_blank">Rôana Góes</a> <br/><br/>
-                
-                
-                        
+                <li>
+                    <FontAwesomeIcon icon={faInstagram} />    
                     <a href="https://www.instagram.com/rowgoes/" target="_blank">rowgoes</a>  <br/><br/>
+                </li>
                 
             </ol>
         </footer>
